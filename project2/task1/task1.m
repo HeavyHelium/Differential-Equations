2ffac524613e372a60075526bbb5964718a35e44
Diana Markova <34967260+HeavyHelium@users.noreply.github.com>
2024-01-28 15:28:59 +0200

    add solution

---
+++ project2/task1/task1.m
@@ -0,0 +1,39 @@
+m = 45;
+t = 0 : 0.01 : 0.8;
+
+xx_a = 1 : 0.01 : 3;
+xx_b = 0 : 0.01 : 4;
+
+for k = 0:m
+    yy_b = xx_b .* sin(pi * xx_b) .* sin((k * pi / 4) * xx_b);
+    b(k + 1) = (-8/(k^2 * pi^2)) * trapz(xx_b, yy_b);
+    if isnan(b(k + 1))
+        b(k + 1) = 0;
+    end
+end
+
+for k = 0:m
+    yy_a = (abs(xx_a - 2) - 1) .* sin(k * pi * (1 / 4) * xx_a);
+    a(k + 1) = -b(k + 1) + (1/2) * trapz(xx_a, yy_a);
+    if isnan(a(k + 1))
+        a(k + 1) = 0;
+    end
+end
+
+x = 0 : 0.01 : 4;
+
+
+for i = 1:length(t)
+    u = zeros(size(x)); 
+    % calculate partial sum
+    for k = 1:m
+        u = u + (a(k) * exp(-((k * pi / 4)^2) * t(i)) + b(k)) * sin((k * pi/4) * x);
+    end
+    plot(x, u);
+     axis([0,4,-1,1])
+    M(i) = getframe;
+end
+
+movie(M, 3);
+
+
---
+++ project2/task2/solution.m
@@ -0,0 +1,45 @@
+
+h = 0.1 ;
+tau = 0.004;
+
+x = 0 : h : 3;
+
+t = 0 : tau : 3;
+
+M = length(t);
+N = length(x);
+
+u = zeros([N, M]);
+
+
+for m = 1 : M
+    u(N, m) = exp(-t(m)) * sin(7 * t(m));
+    u(N, m) = 0;
+end
+
+
+
+for n = 1 : N
+    if x(n) > 0 && x(n) <= 1
+        u(n, 1) = sin(pi * x(n));
+    elseif x(n) < 3 && x(n) > 1
+        u(n, 1) = 0;
+    else 
+         "error";
+    end
+end
+
+
+for m = 1 : M - 1
+    for n = 2 : N - 1
+        u(n, m + 1) = u(n, m) + tau / h^2 * (u(n + 1, m) - 2 * u(n, m) + u(n - 1, m));
+    end
+end
+
+for m = 1 : M
+    plot(x, u(:, m))
+    axis([0, 3, -2, 2])
+    MM(m) = getframe;
+end
+
+movie(MM, 2, 3)
